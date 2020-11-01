5f594ad32f22a240dac62f1a82a53f6fdfeded38
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-01 09:19:28 -0500

    delete boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default MyApp
+export default App
